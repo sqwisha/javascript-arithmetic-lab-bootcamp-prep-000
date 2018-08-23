18fae39ac862267d31b304ea5cd359a8b241429d
Sasha Hart <sashanicolehart@gmail.com>
2018-08-23 01:10:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ function add(a, b) {
    return a + b;
 }
 
-function sub
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
